707e3c2f1cfd5eb2e6c9989aee3a398b3d278119
EC2 Default User <ec2-user@ip-172-31-29-82.ec2.internal>
2021-01-20 10:16:46 +0000

    first commit

--- db/migrate/20210118105701_add_category_to_blogs.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryToBlogs < ActiveRecord::Migration[5.2]
-  def change
-    add_column :blogs, :category, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -20,4 +20,11 @@ ActiveRecord::Schema.define(version: 2021_01_18_105832) do
     t.string "body"
   end
 
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
